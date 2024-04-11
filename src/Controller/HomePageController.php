e6a1d78b166b16681c5181185b5c03c81c8abfbc
Mateusz Pieła <mateuszpiela@users.noreply.github.com>
2024-04-12 01:04:02 +0200

    Add landing page with configured parameters,translations and add bootstrap with icons

---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomePageController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    #[Route('/index', name:'app_home_index')]
+    public function index(): Response
+    {
+        return $this->render('home_page/index.html.twig', [
+            'controller_name' => 'HomePageController',
+        ]);
+    }
+}
